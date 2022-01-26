e695a1944f430d6208c3df43c10e58e5da24765e
Kiono78 <40069205+Kiono78@users.noreply.github.com>
2022-01-26 15:58:25 +0100

    linking the command to the ENV PATH

--- error.c
+++ error.c
@@ -6,8 +6,18 @@ void	file_error(char *error_msg)
 	exit(1);
 }
 
-int	arg_error(char *err_msg)
+int	return_error(char *err_msg)
 {
-	perror(err_msg);
+	write(2, err_msg, ft_strlen(err_msg));
 	return (1);
+}
+
+void	free_exit(t_pipex *pipex)
+{
+	int	i;
+
+	while (pipex->paths[i])
+		free(pipex->paths[i]);
+	free(pipex);
+	exit(1);
 }
--- pipex.c
+++ pipex.c
@@ -10,15 +10,70 @@ int	open_file(char *file_name, int	rights)
 	return (fd);
 }
 
-int main(int argc, char **argv)
+char	**get_paths(char **envp)
+{
+	char			**output;
+	int				i;
+
+	i = 0;
+	while (ft_strncmp(envp[i], "PATH=", 5) != 0)
+		i++;
+	output = ft_split(&envp[i][5], ':');
+	return (output);
+}
+
+char	*get_cmd(t_pipex	*pipex)
+{
+	char	*cmd_path;
+	char	*tmp;
+	int		path_bool;
+	int		i;
+
+	i = 0;
+	while(pipex->paths[i])
+	{
+		tmp = ft_strjoin(pipex->paths[i], "/");
+		printf("tmp : %s", tmp);
+		cmd_path = ft_strjoin(tmp, pipex->cmd);
+		free(tmp);
+		path_bool = access(cmd_path, X_OK);
+		//going to leak ?
+		if (path_bool == 0)
+			return (cmd_path);
+		i++;
+	}
+	if (path_bool == 0)
+		free(cmd_path);
+	return (cmd_path);
+}
+
+void	execute_commands(t_pipex *pipex, char **argv, char **envp)
+{
+	pipex->pid1 = fork();
+	if (pipex->pid1 == -1)
+		free_exit(pipex);
+	if (pipex->pid1 == 0)
+	{
+		pipex->cmd_args = ft_split(argv[2], ' ');
+		printf("cmd_args[0]: %s\n", pipex->cmd_args[0]);
+		pipex->cmd = get_cmd(pipex);
+		execve(pipex->cmd, &pipex->cmd_args[1], envp);
+	}
+}
+
+int main(int argc, char *argv[], char *envp[])
 {
 	t_pipex	pipex;
 
-	//No error follows on windows
 	if (argc != 5)
-		return (arg_error(ARG_ERROR));
+		return (return_error(ARG_ERROR));
 	pipex.fd[0] = open_file(argv[1], O_RDONLY);
-	//could not be tested on windows
 	pipex.fd[1] = open_file(argv[argc - 1], O_TRUNC | O_CREAT | O_RDWR);
+	pipex.channel = pipe(pipex.fd);
+	if (pipex.channel == -1)
+		return (return_error(PIPE_ERROR));
+	pipex.paths = get_paths(envp);
+
+	execute_commands(&pipex, argv, envp);
 	return (0);
 }
